b0068e9b50f044313268dfb96fe915a41c5b1792
tkrain23 <tk.rain09@gmail.com>
2019-02-28 01:39:26 -0800

    create category button component

---
+++ src/components/CategoryButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const CategoryButton = (props) => (
+    <Button>{props.title}</Button>
+)
+
+
+export default CategoryButton
